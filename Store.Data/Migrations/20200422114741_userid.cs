6eb955b0ea08c2cda149e4084d3146d990f6a479
‘ZhangQuequegit config --global user.name ‘ZhangQueque <3393597524@qq.com>
2020-04-22 20:12:22 +0800

    商品用户迁移增加地址店铺

---
+++ Store.Data/Migrations/20200422114741_userid.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Store.Data.Migrations
+{
+    public partial class userid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "UserInfos");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ShippingAddress",
+                table: "UserInfos",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShippingAddress",
+                table: "UserInfos");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "UserInfos",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+        }
+    }
+}
